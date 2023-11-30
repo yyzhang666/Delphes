1d254cd835ee5824498f294c15709121a3538ac5
yyzhang666 <150924556+yyzhang666@users.noreply.github.com>
2023-11-30 02:40:06 +0000

    Delphes_example1_edm4hep

--- Example1.C
+++ Example1.C
@@ -26,7 +26,7 @@ void Example1()
   //TClonesArray *branchJet = treeReader->UseBranch("Jet");
   //TClonesArray *branchElectron = treeReader->UseBranch("Electron");
   //TClonesArray *branchEvent = treeReader->UseBranch("Event");
-  const std::vector<std::string> inputFile={"Delphes_events_edm4hep.root"}
+  const std::vector<std::string> inputFile={"delphes_events_edm4hep.root"};
 
   auto reader = podio::ROOTFrameReader();
   reader.openFiles(inputFile);
@@ -82,7 +82,7 @@ void Example1()
         event.get<edm4hep::ReconstructedParticleCollection>("Jet");
     if(jets.size() > 0)
     {
-      auto jet_pt=edm4hep::utils::p4(jets[0])
+      auto jet_pt=edm4hep::utils::p4(jets[0]);
 
       // Plot jet transverse momentum
       histJetPT->Fill(jet_pt.Pt());
