69ce6bbf9f9ea06855a92c8e294309c2473a59d8
yyzhang666 <150924556+yyzhang666@users.noreply.github.com>
2024-01-20 21:01:13 +0000

    Update zh_mumubb_angle.cpp

--- Higgs_cp_analysis/zh_mumubb_angle.cpp
+++ Higgs_cp_analysis/zh_mumubb_angle.cpp
@@ -18,7 +18,7 @@
 #include <vector>
 #include<iostream>
 
-int main(){
+void zh_mumubb_angle(){
     //Read files
     auto reader = podio::ROOTFrameReader();
     reader.openFile("higgs_recoil_mumubb.root");
@@ -26,38 +26,51 @@ int main(){
     const auto e_cms = edm4hep::LorentzVectorE(0, 0, 0, 250.);
 
     //book histograms
-    auto azimuthal= new TH1D("azimuthal_angle","Phi/rad",100, 0.0, 100.0);
+    auto azimuthal= new TH1D("azimuthal_angle_mb","Phi/rad",100, 0.0, 3.2);
+
+    //Define 3d vector
+    struct Vector3D {
+    double x, y, z;
+    Vector3D(double x, double y, double z) : x(x), y(y), z(z) {}
+	};
     
 
     //loop over events
     for (size_t i = 0; i < reader.getEntries("events"); ++i) {
                auto event = podio::Frame(reader.readNextEntry("events"));
                    const auto &muons =
-        event.get<edm4hep::ReconstructedParticleCollection>("Muons");
+        event.get<edm4hep::ReconstructedParticleCollection>("Muon");
 			if (muons.size()==2){
                 const auto mu1 = edm4hep::utils::p4(muons[0]);
                 const auto mu2 = edm4hep::utils::p4(muons[1]);
 
-                //不确定edm4hep里有没有boost这个方法，要先转成root的TLorentz格式
+		const auto Z_boson=mu1+mu2;
+                const auto H_boson=e_cms-Z_boson;
+				
+                //Change the format to TLorentzVector for transforming the reference
+                TLorentzVector TZ_boson(Z_boson.Px(),Z_boson.Py(),Z_boson.Pz(),Z_boson.E());
+		TLorentzVector TH_boson(H_boson.Px(),H_boson.Py(),H_boson.Pz(),H_boson.E())
+                TLorentzVector Tmu1(mu1.Px(),mu1.Py(),mu1.Pz(),mu1.E());
 
-                TLorentzVector Z_boson=mu1+mu2;
-                TLorentzVector H_boson=e_cms-Z_boson
+                TVector3 boostZ=-TZ_boson.BoostVector();
+		TVector3 boostH=-TH_boson.BoostVector();
+		//Boost mu to Z rest frame
+                Tmu1.Boost(boostZ);
 
-                TVector3 boostZ=-Z_boson.BoostVector();
-                TVector3 boostH=-H_boson.BoostVector();
-
-                mu1.Boost(boostZ);
-                const auto p_mu1=edm4hep::Vector3D(mu1.Px,mu1.Py,mu1.Pz);//再查一下语法
+                Vector3D p_mu1(Tmu1.Px(),Tmu1.Py(),Tmu1.Pz());
 
                 const auto &jets =event.get<edm4hep::ReconstructedParticleCollection>("Jet");
                 if (jets.size()>0){
+			//loop over jets with btag
                     for (const auto &jet:jets){
                         if (jet.BTag&1==1){
-                            const auto b=edm4hep::utils::p4(jet)
-                            TLorentzVector ;
-                            b.Boost(boostH);
-                            auto p_b=edm4hep::Vector3D(b.Px,b.Py,b.Pz);
+                            const auto b=edm4hep::utils::p4(jet);
+                            TLorentzVector Tb(b.Px(),b.Py(),b.Pz(),b.E()); 
+			    //boost b-quark to Higgs rest frame
+                            Tb.Boost(boostH);
+                            Vector3D p_b(Tb.Px(),Tb.Py(),Tb.Pz());
 
+			    //The angle between mu in Z and b in Higgs is the azimuthal angle
                             angle=edm4hep::utils::angleBetween(p_mu1,p_b);
 
                             azimuthal->Fill(angle);
@@ -74,14 +87,8 @@ int main(){
             }
             
 		}
+	auto hist_file= new TFile("higgs_mb_azimuthal.root","recreate");
+	azimuthal->Write();
+	hist_file->Close();
 }
-//读取文件，创建树，book柱状图，
-//遍历事件，筛选出合适的事件
-    //!!!btag在edm4hep里没有，必要时请引入delphes lib
-//用快度方法，分别换到Z静止参考系，和H静止参考系，得到极角，两个极角相减，得到方位角
-    //edm4hep看起来没有直接的方法算角和转换参考系，需要include一些root库，但是有函数可以算极角和eta之间的转换，也可以通过两个动量算方位角
-    //所以应该 1.把b和mu分别转换到各自妈妈的参考系中得到动量（写一个函数）
-            //2.用edm4hep的方位角方法得到方位角
-//cross section先放一下，画方位角的直方图就可以了
-//最后得到柱状图
-//！！柱状图的写入逻辑还需要再疏通一下
+
