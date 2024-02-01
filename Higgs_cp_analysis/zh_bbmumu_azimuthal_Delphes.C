321ecb2f351d60a8ec0454707c8ba0f243c06ed9
yyzhang666 <150924556+yyzhang666@users.noreply.github.com>
2024-02-01 02:16:29 +0000

    Update zh_bbmumu_azimuthal_Delphes.C

--- Higgs_cp_analysis/zh_bbmumu_azimuthal_Delphes.C
+++ Higgs_cp_analysis/zh_bbmumu_azimuthal_Delphes.C
@@ -28,12 +28,11 @@ void Example1(const char *inputFile)
 
   // Get pointers to branches used in this analysis
   TClonesArray *branchJet = treeReader->UseBranch("Jet");
-  TClonesArray *branchElectron = treeReader->UseBranch("Electron");
+  TClonesArray *branchMuon = treeReader->UseBranch("Muon");
   TClonesArray *branchEvent = treeReader->UseBranch("Event");
 
   // Book histograms
-  TH1 *histJetPT = new TH1F("jet_pt", "jet P_{T}", 100, 0.0, 100.0);
-  TH1 *histMass = new TH1F("mass", "M_{inv}(e_{1}, e_{2})", 100, 40.0, 140.0);
+  TH1 *histAzimuthal = new TH1F("b_mu azimuthal angle", "Phi/rad", 100, 0.0, 1.6);
 
   // Loop over all events
   for(Int_t entry = 0; entry < numberOfEntries; ++entry)
@@ -47,7 +46,9 @@ void Example1(const char *inputFile)
     //Float_t weight = event->Weight;
 
     // If event contains at least 1 jet
-    if(branchJet->GetEntries() > 0)
+    if(branchJet->GetEntries() != 2){
+      continue;
+    }else
     {
       // Take first jet
       Jet *jet = (Jet*) branchJet->At(0);
@@ -59,17 +60,17 @@ void Example1(const char *inputFile)
       cout << "Jet pt: "<<jet->PT << endl;
     }
 
-    Electron *elec1, *elec2;
+    Muon *mu1, *mu2;
 
-    // If event contains at least 2 electrons
-    if(branchElectron->GetEntries() > 1)
+    // If event contains at least 2 muons
+    if(branchMuon->GetEntries() > 1)
     {
-      // Take first two electrons
-      elec1 = (Electron *) branchElectron->At(0);
-      elec2 = (Electron *) branchElectron->At(1);
+      // Take first two muons
+      mu1 = (Muon *) branchMuon->At(0);
+      mu2 = (Muon *) branchMuon->At(1);
 
-      // Plot their invariant mass
-      histMass->Fill(((elec1->P4()) + (elec2->P4())).M());
+      // Boost them in Z rest frame
+      //histMass->Fill(((elec1->P4()) + (elec2->P4())).M());
     }
   }
 
